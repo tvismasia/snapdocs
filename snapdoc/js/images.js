24827cf5d91366fb7231bd161f789777c1f9ddf4
Tebogo Masia <49418390+tvismasia@users.noreply.github.com>
2023-10-14 03:09:39 +0200

    Add files via upload

---
+++ snapdoc/js/images.js
@@ -0,0 +1,17 @@
+var image = document.getElementById('my-img');
+    
+    function lender() {
+      image.src = 'img/lender-accordion-eVault.png';
+    }
+
+    function escrow() {
+      image.src = 'img/title-escrow.png';
+    }
+
+    function signings() {
+      image.src = 'img/scheduling.png';
+    }
+
+    function notary() {
+      image.src = 'img/notary-signing-agents.png';
+    }
